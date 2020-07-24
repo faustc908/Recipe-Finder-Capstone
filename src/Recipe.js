42587f9ee16eff1286938ec047d8312048d9a947
faustc908 <57776835+faustc908@users.noreply.github.com>
2020-07-24 15:26:46 -0400

    Update Recipe.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -4,16 +4,16 @@ import style from './recipe.module.css;
 const Recipe = ({title, calories, image, ingredients}) => {
     return(
     <div className={style.recipe}>
-    <h1>{title}<h1>
+     <h1>{title}<h1>
         <ol>
         {ingredients.map(ingredient =>(
          <li>{ingredient.text}</li>
          ))};
         </ol>
-    <p>{calories}</p>
-    <img className={style.image} src={image} alt=''/>
+     <p>{calories}</p>
+      <img className={style.image} src={image} alt=''/>
     </div>
     );
-};
+   };
 
 export default Recipe
