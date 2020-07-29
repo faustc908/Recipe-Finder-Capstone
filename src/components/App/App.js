3ccd64e3904a7399235c8fe6e9c362b9185ddfde
faustc908 <57776835+faustc908@users.noreply.github.com>
2020-07-29 17:57:43 -0400

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,78 +1,55 @@
-import React,{useEffect, useState} from 'react';
-import './App.css';
-import Recipe from '../Recipe/Recipe';
-import NavBar from '../NavBar/NavBar';
-import Footer from '../Footer/Footer';
-import Appdesc from '../Appdesc/Appdesc';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Recipe from "../Recipe/Recipe";
+import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
+import Appdesc from "../Appdesc/Appdesc";
+import AppForm from "../AppForm/AppForm";
 // API call & State
 
- const App = () => {
-    const APP_ID = '7e817b93';
-    const APP_KEY = 'dd1f3f5c93f40c501dd29392c73cbd2d';
-  
-    const [recipes, setRecipes] = useState([]);
-    const [search, setSearch] = useState('');
-    const [query, setQuery] = useState('banana');
+const App = () => {
+  const APP_ID = "7e817b93";
+  const APP_KEY = "dd1f3f5c93f40c501dd29392c73cbd2d";
 
- useEffect(() => {
-    getRecipes();
- }, [query]);
+  const [recipes, setRecipes] = useState([]);
+  const [query, setQuery] = useState("banana");
 
- const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const data = await response.json();
-    setRecipes(data.hits);
-    console.log(data.hits);
- };
- 
- // Updating Search and Search
-
-  const updateSearch = e => {
-  setSearch(e.target.value);
-  };
-  
-  const getSearch = e => {
-  e.preventDefault();
-  setQuery(search);
-  setSearch('');
-  }
+  //When query updates call API to get recipes.
+   
+  useEffect(() => {
+    fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => setRecipes(data.hits));
+  }, [query, setRecipes]);
 
   // Render
 
-   return(
-     <div className='App'>
+  return (
+    <div className="App">
       <NavBar />
       <Appdesc />
-      <p className='formHead'>Please enter an ingredient:</p>
-         <form onSubmit={getSearch} className='search-form'>
-         <input 
-         className='search-bar' 
-         type='text' 
-         placeholder='Banana'
-         value={search} 
-         onChange={updateSearch}
-         required/>
-      <button className='search-button' type='submit'>
-        Search
-      </button>
-     </form>
-      <div className='recipes' >
-      {recipes.map(recipe => (
-       <Recipe
-       key={recipe.recipe.label}
-       title={recipe.recipe.label} 
-       calories={recipe.recipe.calories}
-       image={recipe.recipe.image}
-       ingredients={recipe.recipe.ingredients}
-       url={recipe.recipe.url}
-     />
-    ))}
+      <p className="formHead">Please enter an ingredient:</p>
+      {/* Pure component using an event to notify parent */}
+      <AppForm onSearch={setQuery} />
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+            url={recipe.recipe.url}
+          />
+        ))}
       </div>
-       <div>
+      <div>
         <Footer />
-       </div>
-     </div>
-   )}
+      </div>
+    </div>
+  );
+};
 
 export default App;
